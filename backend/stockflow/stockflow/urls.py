b51ee979035acd82ef3e584c3ca34c190cdda1a0
soryinza <soryinzadiomande@gmail.com>
2026-05-10 12:05:07 +0000

    creation backend

---
+++ backend/stockflow/stockflow/urls.py
@@ -0,0 +1,29 @@
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+
+    path(
+        'admin/',
+        admin.site.urls
+    ),
+
+    path(
+        'api/',
+        include('inventaire.urls')
+    ),
+
+    path(
+        'api/',
+        include('commandes.urls')
+    ),
+
+    path(
+        'api/',
+        include('dashboard.urls')
+    ),
+    path(
+    'api/',
+    include('statistiques.urls')
+),
+]
